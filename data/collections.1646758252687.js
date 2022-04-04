20b5eb90b404f87620b006770fdde89f08b65289
dave mitchell <dave@connectedbits.com>
2022-04-04 15:06:57 -0400

    update

--- data/collections.1646758252687.js
+++
@@ -1,1 +0,0 @@
-window.collectionsJSON = [{"books":["B00JQLTE2O"],"id":"fa109f52-60a7-4b32-875c-804a740e971e","title":"Return"},{"books":["0593458826","B01LW7OXWP","B002V00VA6","B002V0828O","B002V1C3R4"],"description":"Your favorite listens","id":"__FAVORITES","title":"Favorites"}];
